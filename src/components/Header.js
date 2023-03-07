9e7c2f366ff419b2904156576f782ac64a7096d1
Santiago <santoscastelvi2014@gmail.com>
2023-03-07 15:32:12 -0300

    primer commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = ({title}) => {
+    return ( 
+        <header>
+            <div className="container">
+                <div className="content">
+                    <h1 className="title">{title}</h1>
+                    <p className="text">This project illustrate the characters of the famous Rick and Morty Series</p>
+                </div>
+            </div>
+        </header>
+     );
+}
+
+export default Header
